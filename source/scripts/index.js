f79f161a03365d93f9fb6de2da7d4277e3c9d412
Vladimir Redkin <3xvstudio@gmail.com>
2024-01-29 16:12:36 +0500

    доработки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -11,3 +11,19 @@ const menuOpen = () => {
 };
 
 $toggle.addEventListener('click', () => menuOpen());
+
+/* валидация формы */
+const $form = document.querySelector('.form');
+const $input = document.querySelectorAll('input');
+const $button = document.querySelector('.button');
+
+$button.addEventListener('click', () => validate());
+const validate = () => {
+  if (!$form.checkValidity()) {
+    $input.forEach(input => {
+      if (input.hasAttribute('required')) {
+        input.classList.add('form__control--invalid');
+      }
+    });
+  }
+}
