39725ac72ceb4cb6aae3b208f01e780fd9bfc0e1
Vladimir Redkin <3xvstudio@gmail.com>
2024-01-30 11:31:47 +0500

    исправления после первой проверки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,13 +1,13 @@
 /* в этот файл добавляет скрипты*/
 const $toggle = document.querySelector('.toggle');
-const $mainMenu = document.querySelector('.main-menu');
+const $mainMenu = document.querySelector('.main-nav__list');
 
 $toggle.classList.remove('main-nav__toggle--nojs', 'toggle--open');
-$mainMenu.classList.remove('main-menu--nojs', 'main-menu--open');
+$mainMenu.classList.remove('main-nav__list--nojs', 'main-nav__list--open');
 
 const menuOpen = () => {
   $toggle.classList.toggle('toggle--open');
-  $mainMenu.classList.toggle('main-menu--open');
+  $mainMenu.classList.toggle('main-nav__list--open');
 };
 
 $toggle.addEventListener('click', () => menuOpen());
@@ -27,3 +27,26 @@ const validate = () => {
   }
 };
 $button.addEventListener('click', () => validate());
+
+// function getTextNodesIn(elem, opt_fnFilter) {
+//   var textNodes = [];
+//   if (elem) {
+//     for (var nodes = elem.childNodes, i = nodes.length; i--;) {
+//       var node = nodes[i],
+//         nodeType = node.nodeType;
+//       if (nodeType == 3) {
+//         if (!opt_fnFilter || opt_fnFilter(node, elem)) {
+//           if (node.data.trim() !== '') {
+//             textNodes.push(node.data.trim() !== '' ? node : '');
+
+//           }
+//         }
+//       } else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
+//         textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
+//       }
+//     }
+//   }
+//   return textNodes;
+// }
+
+// getTextNodesIn(document.body).forEach((item) => { item.textContent = "У меня есть несколько клиентов, которые записались ко мне на стрижку, не успев даже зайти в салон." })
